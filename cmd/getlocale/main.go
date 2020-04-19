6cc3050958abaf2abf46974d313d480eb2cdfafe
Alexis Jeandeau <alexis.jeandeau@gmail.com>
2020-04-19 22:01:26 +0900

    Add a command-line application to retrieve the locale information

---
+++ cmd/getlocale/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jeandeaual/go-locale"
+)
+
+func main() {
+	userLocale, err := locale.GetLocale()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Couldn't retrieve the locale: %v\n", err)
+	} else {
+		fmt.Println("Locale:", userLocale)
+	}
+
+	language, err := locale.GetLanguage()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Couldn't retrieve the language: %v\n", err)
+	} else {
+		fmt.Println("Language:", language)
+	}
+
+	region, err := locale.GetRegion()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Couldn't retrieve the region: %v\n", err)
+	} else {
+		fmt.Println("Region:", region)
+	}
+
+	locales, err := locale.GetLocales()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Couldn't retrieve the locales: %v\n", err)
+	} else {
+		fmt.Println("Locales:", locales)
+	}
+}
