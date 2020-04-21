dc67be454f5b21a6a0c5914ac0eb53a5e1842103
Alexis Jeandeau <alexis.jeandeau@gmail.com>
2020-04-21 21:57:31 +0900

    Add tests for splitLocale

---
+++ util_test.go
@@ -0,0 +1,25 @@
+package locale
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSplitLocale(t *testing.T) {
+	language, region := splitLocale("en_US.UTF-8")
+	assert.Equal(t, "en", language)
+	assert.Equal(t, "US", region)
+
+	language, region = splitLocale("fr-FR")
+	assert.Equal(t, "fr", language)
+	assert.Equal(t, "FR", region)
+
+	language, region = splitLocale("ja")
+	assert.Equal(t, "ja", language)
+	assert.Equal(t, "", region)
+
+	language, region = splitLocale("test")
+	assert.Equal(t, "test", language)
+	assert.Equal(t, "", region)
+}
