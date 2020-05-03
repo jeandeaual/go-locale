4e79c8f8372859c1b040c77b6d0b5e9bc4ef6d1c
Alexis Jeandeau <alexis.jeandeau@gmail.com>
2020-05-04 00:55:59 +0900

    Localize getlocale-mobile

--- cmd/getlocale-mobile/main.go
+++ cmd/getlocale-mobile/main.go
@@ -6,14 +6,14 @@ import (
 
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
+	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"golang.org/x/text/language"
 
 	"github.com/jeandeaual/go-locale"
 )
 
 func main() {
-	a := app.New()
-
-	locales, err := locale.GetLocales()
+	userLocales, err := locale.GetLocales()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Couldn't retrieve the locales: %v\n", err)
 	}
@@ -23,7 +23,7 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Couldn't retrieve the locale: %v\n", err)
 	}
 
-	language, err := locale.GetLanguage()
+	lang, err := locale.GetLanguage()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Couldn't retrieve the language: %v\n", err)
 	}
@@ -33,12 +33,87 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Couldn't retrieve the region: %v\n", err)
 	}
 
+	bundle := i18n.NewBundle(language.English)
+	bundle.AddMessages(
+		language.French,
+		&i18n.Message{
+			ID:    "Language",
+			Other: "Langue: {{.Language}}",
+		},
+		&i18n.Message{
+			ID:    "Locale",
+			Other: "Locale: {{.Locale}}",
+		},
+		&i18n.Message{
+			ID:    "Locales",
+			Other: "Locales: {{.Locales}}",
+		},
+		&i18n.Message{
+			ID:    "Region",
+			Other: "Région: {{.Region}}",
+		},
+	)
+	bundle.AddMessages(
+		language.Japanese,
+		&i18n.Message{
+			ID:    "Language",
+			Other: "言語： {{.Language}}",
+		},
+		&i18n.Message{
+			ID:    "Locale",
+			Other: "ロケール： {{.Locale}}",
+		},
+		&i18n.Message{
+			ID:    "Locales",
+			Other: "ロケール： {{.Locales}}",
+		},
+		&i18n.Message{
+			ID:    "Region",
+			Other: "領域： {{.Region}}",
+		},
+	)
+	localizer := i18n.NewLocalizer(bundle, userLocales...)
+
+	a := app.New()
+
 	w := a.NewWindow("Locale Information")
 	w.SetContent(widget.NewVBox(
-		widget.NewLabel(fmt.Sprintf("Locale: %s", userLocale)),
-		widget.NewLabel(fmt.Sprintf("Language: %s", language)),
-		widget.NewLabel(fmt.Sprintf("Region: %s", region)),
-		widget.NewLabel(fmt.Sprintf("Locales: %v", locales)),
+		widget.NewLabel(localizer.MustLocalize(&i18n.LocalizeConfig{
+			DefaultMessage: &i18n.Message{
+				ID:    "Locales",
+				Other: "Locales: {{.Locales}}",
+			},
+			TemplateData: map[string]interface{}{
+				"Locales": userLocales,
+			},
+		})),
+		widget.NewLabel(localizer.MustLocalize(&i18n.LocalizeConfig{
+			DefaultMessage: &i18n.Message{
+				ID:    "Language",
+				Other: "Language: {{.Language}}",
+			},
+			TemplateData: map[string]interface{}{
+				"Language": lang,
+			},
+		})),
+		widget.NewLabel(localizer.MustLocalize(&i18n.LocalizeConfig{
+			DefaultMessage: &i18n.Message{
+				ID:    "Region",
+				Other: "Region: {{.Region}}",
+			},
+			TemplateData: map[string]interface{}{
+				"Region": region,
+			},
+		})),
+		widget.NewLabel(localizer.MustLocalize(&i18n.LocalizeConfig{
+			DefaultMessage: &i18n.Message{
+				ID:    "Locale",
+				Other: "Locale: {{.Locale}}",
+			},
+			TemplateData: map[string]interface{}{
+				"Locale": userLocale,
+			},
+		})),
 	))
 
 	w.ShowAndRun()
--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.12
 require (
 	fyne.io/fyne v1.2.4
 	github.com/fyne-io/mobile v0.0.1
+	github.com/nicksnyder/go-i18n/v2 v2.0.3
 	github.com/stretchr/testify v1.5.1
-	golang.org/x/sys v0.0.0-20200420163511-1957bb5e6d1f
+	golang.org/x/sys v0.0.0-20200501145240-bc7a7d42d5c3
+	golang.org/x/text v0.3.2
 )
